37e7a9a9743884261d3df0ac5f068120abd2ffac
cnwilliams <hello.christenwilliams@gmail.com>
2017-01-29 19:12:40 -0800

    new assets added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,38 @@
 // sanity check
-console.log("app.js linked.");
+alert("app.js linked.");
+
+var myWorkObject = [
+  {name: "example 1",
+  picture: "example.jpg",
+  description: "This is a sample of my work that was created while learning how to use Bootstrap and jQuery.",
+  link: "examplelink.com"
+  },
+
+  {name: "example 2",
+  picture: "example.jpg",
+  description: "Here is another sample of my work.",
+  link: "examplelink.com"},
+
+  {name: "example 3",
+  picture: "example.jpg",
+  description: "Here is another sample of my work.",
+  link: "examplelink.com"}
+];
+
+
+
+for (var i = 0; i < myWorkObject.length; i++){
+
+  var title = myWorkObject[i].name;
+  var image = myWorkObject[i].picture;
+  var descr = myWorkObject[i].description;
+  var url = myWorkObject[i].link
+  // template text to create image
+
+  // create title
+
+  // create and add decription
+
+  // cratelink
+
+}
